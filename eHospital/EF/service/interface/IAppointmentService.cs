ed8afadb8033e3521fee28f4d20cf0ce77278a8e
Orik25 <orestpilavka@gmail.com>
2023-11-07 19:11:21 +0200

    connected to db and added all needed methods

---
+++ eHospital/EF/service/interface/IAppointmentService.cs
@@ -0,0 +1,22 @@
+﻿using EF.DTO.Appointment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.service
+{
+    internal interface IAppointmentService
+    {
+        Appointment FindById(long id);
+        void AddNew(AppointmentDTO appointment);
+        void Update(AppointmentDTO AppointmentDTO);
+        void ArchiveById(long id);
+        void DeleteById(long appointment);
+        List<Appointment> GetArchiveAppointmentsByUserId(long id);
+        List<Appointment> GetActiveAppointmentsByUserId(long id);
+        List<Appointment> GetAppointmentsByUserId(long id);
+        long GetNumberOfAppointments();
+    }
+}
