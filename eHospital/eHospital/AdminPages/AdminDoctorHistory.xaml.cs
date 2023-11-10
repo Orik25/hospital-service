d755d9d2aa0fd916228f5541d2d23869d9e56a9b
Bohdan Pyrohanych <bohdanpyrohanych@gmail.com>
2023-11-10 10:15:47 +0200

    <Required>

---
+++ eHospital/eHospital/AdminPages/AdminDoctorHistory.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eHospital.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for AdminDoctorHistory.xaml
+    /// </summary>
+    public partial class AdminDoctorHistory : Page
+    {
+        public AdminDoctorHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
